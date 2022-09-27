9142eb9af2f945cf0e4359504e2500fe0c46c49e
MuhAsh99 <mjashraf@smu.edu>
2022-09-27 10:08:31 -0500

    Initial Commit

---
+++ AssignmentTwo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AssignmentTwo
+//
+//  Created by Muhammad Ashraf on 9/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
