be6f41079768d2745e62c5494c9c7bba41d32266
MuhAsh99 <mjashraf@smu.edu>
2022-10-06 18:03:50 -0500

    AudioModel.swift
    added in functions for creating sin-wave

--- AssignmentTwo/AudioModel.swift
+++ AssignmentTwo/AudioModel.swift
@@ -13,11 +13,15 @@ class AudioModel {
     
     // MARK: Properties
     private var BUFFER_SIZE:Int
+    //this is for the sin wave
+    private let USE_C_SINE = false
     // thse properties are for interfaceing with the API
     // the user can access these arrays at any time and plot them if they like
     var timeData:[Float]
     var fftData:[Float]
     var highestFreq:[Float]
+    
+
     
     // MARK: Public Methods
     init(buffer_size:Int) {
@@ -27,6 +31,26 @@ class AudioModel {
         fftData = Array.init(repeating: 0.0, count: BUFFER_SIZE/2)
         highestFreq = Array.init(repeating: 0.0, count: BUFFER_SIZE/2)
     }
+    
+    //using this for creating an in-audible sound
+    func startProcessingSinewaveForPlayback(withFreq:Float=330.0){
+            sineFrequency = withFreq
+            // Two examples are given that use either objective c or that use swift
+            //   the swift code for loop is slightly slower thatn doing this in c,
+            //   but the implementations are very similar
+            if let manager = self.audioManager{
+
+                if USE_C_SINE {
+                    // c for loop
+                    manager.setOutputBlockToPlaySineWave(sineFrequency)
+                }else{
+                    // swift for loop
+                    manager.outputBlock = self.handleSpeakerQueryWithSinusoid
+                }
+
+
+            }
+        }
     
     // public function for starting processing of microphone data
     func startMicrophoneProcessing(withFps:Double){
@@ -102,6 +126,44 @@ class AudioModel {
         // copy samples from the microphone into circular buffer
         self.inputBuffer?.addNewFloatData(data, withNumSamples: Int64(numFrames))
     }
+    
+    var sineFrequency:Float = 0.0 { // frequency in Hz (changeable by user)
+            didSet{
+                
+                if let manager = self.audioManager {
+                    if USE_C_SINE {
+                        // if using objective c: this changes the frequency in the novocaine block
+                        manager.sineFrequency = sineFrequency
+                        
+                    }else{
+                        // if using swift for generating the sine wave: when changed, we need to update our increment
+                        phaseIncrement = Float(2*Double.pi*Double(sineFrequency)/manager.samplingRate)
+                    }
+                }
+            }
+        }
+        
+        // SWIFT SINE WAVE
+        // everything below here is for the swift implementation
+        // this can be deleted when using the objective c implementation
+        private var phase:Float = 0.0
+        private var phaseIncrement:Float = 0.0
+        private var sineWaveRepeatMax:Float = Float(2*Double.pi)
+        
+        private func handleSpeakerQueryWithSinusoid(data:Optional<UnsafeMutablePointer<Float>>, numFrames:UInt32, numChannels: UInt32){
+            // while pretty fast, this loop is still not quite as fast as
+            // writing the code in c, so I placed a function in Novocaine to do it for you
+            // use setOutputBlockToPlaySineWave() in Novocaine
+            if let arrayData = data{
+                var i = 0
+                while i<numFrames{
+                    arrayData[i] = sin(phase)
+                    phase += phaseIncrement
+                    if (phase >= sineWaveRepeatMax) { phase -= sineWaveRepeatMax }
+                    i+=1
+                }
+            }
+        }
     
     
 }
